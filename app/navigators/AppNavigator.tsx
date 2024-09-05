ab78b80c8c438a5c018773e92a60872e111e91c2
Shashank Maurya <shashank@Shashanks-MacBook-Pro.local>
2024-09-06 01:30:38 +0530

    added drawer icons

--- app/navigators/AppNavigator.tsx
+++ app/navigators/AppNavigator.tsx
@@ -2,15 +2,25 @@ import React from 'react';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
 import { ScannerScreen } from '../screens';
+import { Icon } from '../components';
 
 
 
 const Drawer = createDrawerNavigator();
 
+const drawerIcon = (name: string,color: string | undefined, size: number | undefined) => <Icon name={name} color={color} size={size} />;
+
 const AppDrawer = ()=>{
     return(
-        <Drawer.Navigator>
-            <Drawer.Screen name="Scanner" component={ScannerScreen} options={{headerShown: false}} />
+        <Drawer.Navigator >
+            <Drawer.Screen name="Scan" component={ScannerScreen} options={{headerShown: false, drawerIcon: ({color,size}) => drawerIcon('fullscreen',color,size)}} />
+             <Drawer.Screen name="Scan Image" component={ScannerScreen} options={{headerShown: false, drawerIcon: ({color,size}) => drawerIcon('image',color,size)}} />
+             <Drawer.Screen name="Favorites" component={ScannerScreen} options={{headerShown: false, drawerIcon: ({color,size}) => drawerIcon('grade',color,size)}} />
+            <Drawer.Screen name="History" component={ScannerScreen} options={{headerShown: false, drawerIcon: ({color,size}) => drawerIcon('history',color,size)}} />
+            <Drawer.Screen name="My QR" component={ScannerScreen} options={{headerShown: false, drawerIcon: ({color,size}) => drawerIcon('account-box',color,size)}} />
+             <Drawer.Screen name="Create QR" component={ScannerScreen} options={{headerShown: false, drawerIcon: ({color,size}) => drawerIcon('edit',color,size)}} />
+             <Drawer.Screen name="Settings" component={ScannerScreen} options={{headerShown: false, drawerIcon: ({color,size}) => drawerIcon('settings',color,size)}} />
+             <Drawer.Screen name="Share" component={ScannerScreen} options={{headerShown: false, drawerIcon: ({color,size}) => drawerIcon('share',color,size)}} />
         </Drawer.Navigator>
     );
 };
